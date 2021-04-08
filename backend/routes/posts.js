a61a526324c5d2808489415a05116cc8f9f6e054
Ernesto <ejcustodio@atsistemas.com>
2021-04-08 16:51:49 +0200

    Pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -64,9 +64,10 @@ router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) 
 });
 
 router.get('', (req, res, next) => {
-    const pageSize = +req.query.pageSize;
+    const pageSize = +req.query.pagesize;
     const currentPage = +req.query.page;
     const postQuery = Post.find();
+    let fetchedPosts;
     if(pageSize && currentPage){
         postQuery
             .skip(pageSize * (currentPage - 1))
@@ -74,9 +75,13 @@ router.get('', (req, res, next) => {
     }
     postQuery
         .then(documents => {
+            fetchedPosts = documents;
+            return Post.count();
+        }).then(count => {
             res.status(200).json({
                 message: 'Posts fetched succesfully',
-                posts: documents
+                posts: fetchedPosts,
+                maxPosts: count
             });
         });
 });
