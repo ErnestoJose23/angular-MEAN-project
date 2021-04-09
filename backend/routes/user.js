56e185464808cac36da9bede260d5e8043f50329
Ernesto <ejcustodio@atsistemas.com>
2021-04-09 09:24:39 +0200

    Auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const { response } = require('express');
 const user = require('../models/user');
 
 const router = express.Router();
@@ -7,22 +11,51 @@ const router = express.Router();
 router.post("/signup", (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
-            const user = new user({
+            const user = new User({
                 email: req.body.email,
                 password: hash
         });
-    });
-
-    user.save()
-        .then(result => {
-            res.status(201).json({
-                message: 'User created!',
-                result: result
+        user.save()
+            .then(result => {
+                res.status(201).json({
+                    message: 'User created!',
+                    result: result
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error:err
+                });
             });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user){
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            return bcrypt.compare(req.body.password, user.password)
+        })
+        .then(result => {
+            if(!result){
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            const token = jwt.sign(
+                {email: user.email, userId: user._id}, 
+                'secret_this_should_be_longer', 
+                {expiresIn: "1h"}
+                );
+
         })
         .catch(err => {
-            res.status(500).json({
-                error:err
+            return res.status(401).json({
+                message: "Auth failed"
             });
         });
 });
